76c75cce22c3ca6f7558d30177679fa9a2ed7c50
alejolo311 <alejolo311@gmail.com>
2020-07-19 09:54:18 -0500

    0-lockboxes.py

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def canUnlockAll(boxes):
+    """algorithm to solve the problem of
+    loockboxes"""
+    past = [False for i in range(len(boxes))]
+    past[0] = True
+    st = [0]
+    while len(st):
+        box = st.pop(0)
+        for nBox in boxes[box]:
+            if isinstance(nBox, int):
+                if nBox >= 0 and nBox < len(boxes):
+                    if not past[nBox]:
+                        past[nBox] = True
+                        st.append(nBox)
+    return all(past)
